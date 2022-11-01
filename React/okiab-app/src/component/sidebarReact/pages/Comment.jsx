2687989aae5c913486db5f08da6901c9d3fe49a8
DhanashriMane95 <dmmane6@gmail.com>
2022-11-01 15:36:42 +0530

    sidebar update

---
+++ React/okiab-app/src/component/sidebarReact/pages/Comment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Comment = () => {
+  return (
+    <>
+    <h1>Comment</h1>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, harum.</div>
+    </>
+  )
+}
+
+export default Comment
